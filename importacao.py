ebbe6eede3c28abdf7fd99ad6b62961a921ab4c7
Pedro Henrique <henriquevic012@gmail.com>
2020-09-04 21:58:06 -0300

    Separação do arquivo de acordo com o fornecedor informado pelo usuário

--- importacao.py
+++ importacao.py
@@ -20,7 +20,7 @@ class Importacao:
             'mascara', 'mascrel', 'zebra1', 'tipo_lan',
             'emissao', 'codi_lote', 'nome_fantasia_incorporacao', 'nro_quebra_incorporacao',
             'natureza', 'filial', 'codigo_scp', 'descricao_scp', 'ordem'], inplace=True, axis=1)
-        #Ficam apenas as colunas "nomec", "codic", "datalan", "valdeb", "valcre", "historico"
+        #Ficam apenas as colunas "nomec", "codic", "datalan", "valdeb", "valcre", "historico".
         compras = self.arquivo.loc[(self.arquivo['valcre'] != 0) & (self.arquivo['codic'] == int(self.fornecedor))]
         pagamentos = self.arquivo.loc[(self.arquivo['valdeb'] != 0) & (self.arquivo['codic'] == int(self.fornecedor))]
         saldo_anterior = self.arquivo.loc[(self.arquivo['valdeb'] == 0) & (self.arquivo['valcre'] == 0) & (self.arquivo['codic'] == int(self.fornecedor))]
